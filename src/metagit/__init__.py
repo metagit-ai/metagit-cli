ac925bb2fcad93af4acd3b6e828f6cfd8a89cacc
Zachary Loeber <zloeber@gmail.com>
2025-07-20 22:16:55 -0500

    fix: versioning with setuptools_scm

--- src/metagit/__init__.py
+++ src/metagit/__init__.py
@@ -2,15 +2,23 @@
 """
 Metagit detection tool
 
-.. currentmodule:: metagit_detect
-.. moduleauthor:: Zachary Loeber <zloeber@gmail.com>
+.. currentmodule:: metagit
+.. moduleauthor:: Metagit <zloeber@gmail.com>
 """
 
 import os
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-__version__ = "0.1.2"
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    from setuptools_scm import get_version
+
+    __version__ = get_version(root="..", relative_to=__file__)
+
+
 SCRIPT_PATH = os.path.abspath(os.path.split(__file__)[0])
 CONFIG_PATH = os.getenv(
     "METAGIT_CONFIG", os.path.join(SCRIPT_PATH, (".metagit.config.yml"))
